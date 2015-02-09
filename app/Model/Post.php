44d1b1dfecf66ac1a2eeb69b3898448be7f59ba9
istorm <istorm1022@gmail.com>
2015-02-09 17:35:19 +0000

    DocBlockコメント追加

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -3,12 +3,25 @@ App::uses('AppModel', 'Model');
 /**
  * Post Model
  *
+ * ブログ記事用モデルです
+ *
+ * @copyright php_ci_book
+ * @link https://github.com/phpcibook/blogapp/blob/master/app/Model/Post.php
+ * @since 1.0
+ * @auther 作者名 <test@example.com>
+ *
  */
 class Post extends AppModel {
+
+    /**
+     * 一覧表示のタイトルに使用するカラム名
+     *
+     * @var string
+     */
     public $displayField = 'title';
 
 /**
- * Validation rules
+ * バリデーションルール
  *
  * @var array
  */
